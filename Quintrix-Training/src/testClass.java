404f0c2d203c76b444e35dd10917e4ffcb153c23
arcanlte <pinangayjoel@gmail.com>
2020-10-28 16:44:39 -0400

    Homework done:

---
+++ Quintrix-Training/src/testClass.java
@@ -0,0 +1,10 @@
+
+public class testClass {
+	public static void main(String[] args) {
+		try {
+		System.out.println("Hello World");
+		} catch(Exception e){
+			
+		}
+	}
+}
---
+++ Quintrix-Training/testNG/TestNG.java
@@ -0,0 +1,10 @@
+package testClass;
+
+import org.testng.annotations.Test;
+
+public class TestNG {
+  @Test
+  public static void main(String[] args) {
+	  System.out.println("Hello World");
+  }
+}
