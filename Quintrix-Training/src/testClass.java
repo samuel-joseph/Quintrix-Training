766e3b7e8f556499f4dea879ad044c6593adc22b
arcanlte <pinangayjoel@gmail.com>
2020-10-29 18:02:25 -0400

    Developer branch created and print ArrayList using for loop is successful.

--- Quintrix-Training/src/testClass.java
+++ Quintrix-Training/src/testClass.java
@@ -1,10 +1,11 @@
 
 public class testClass {
 	public static void main(String[] args) {
-		try {
-		System.out.println("Hello World");
-		} catch(Exception e){
-			System.out.print("Test");
+		String[] ArrayList = {"Mark","John","Michael"};
+		
+		for(int i = 0; i<ArrayList.length;i++) {
+			System.out.println(ArrayList[i]);
 		}
+		
 	}
 }
