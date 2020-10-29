6f726b6d9cd16abf095e0a50e31ddb51f0b786cc
arcanlte <pinangayjoel@gmail.com>
2020-10-29 18:05:50 -0400

    for each loop is success. Homework completed.

--- Quintrix-Training/src/testClass.java
+++ Quintrix-Training/src/testClass.java
@@ -3,9 +3,11 @@ public class testClass {
 	public static void main(String[] args) {
 		String[] ArrayList = {"Mark","John","Michael"};
 		
+		for(String arr : ArrayList) {
+			System.out.println(arr);
+		}
 		for(int i = 0; i<ArrayList.length;i++) {
 			System.out.println(ArrayList[i]);
 		}
-		
 	}
 }
