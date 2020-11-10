5f72c5af66afd0b6bcb29dd275daa4f01892ef00
arcanlte <pinangayjoel@gmail.com>
2020-11-10 14:41:39 -0500

    File download is done

--- the-internet/src/test/java/Automation/TheInternet.java
+++ the-internet/src/test/java/Automation/TheInternet.java
@@ -26,147 +26,147 @@ public class TheInternet {
 		Assert.assertEquals(currentURL, url);
 	}
 
-	@Test
-	public void tc1canNavigateToIndexPage() {
-		// Arrange
-		String expectedPageTitle = "The Internet";
-
-		// Act
-		String actualPageTitle = new IndexPage(driver, this.url).navigate().getTitle();
-
-		// Assert
-		Assert.assertEquals(actualPageTitle, expectedPageTitle);
-	}
-
-	@Test
-	public void tc2AddRemoveElement() {
-		// Arrange
-		String expectedSelection = "Delete";
-
-		// Act
-		String selectedOption = new AddRemoveElement(driver, this.url).navigate().select().getOption(expectedSelection);
-		// Assert
-		Assert.assertEquals(selectedOption, expectedSelection);
-	}
-
-	@Test
-	public void tc3BasicAuth() {
-		// Arrange
-		String expectedSelection = "Congratulations! You must have the proper credentials.";
-
-		// Act
-		String selectedOption = new BasicAuth(driver, this.url).navigate().select().getOption();
-
-		// Assert
-		Assert.assertEquals(selectedOption, expectedSelection);
-	}
-
-	@Test
-	public void tc4ChallengingDom() {
-		// Arrange
-		String expectedSelection = "Challenging DOM";
-
-		// Act
-		String selectedOption = new ChallengingDom(driver, this.url).navigate().select().getOption();
-
-		// Assert
-		Assert.assertEquals(selectedOption, expectedSelection);
-
-	}
-	
-	@Test
-	public void tc6ContextMenu() {
-		//Arrange
-		String expectedSelection = "Context Menu";
-		
-		//Act
-		String selectedOption = new ContextMenu(driver, this.url)
-				.navigate()
-				.select()
-				.getOption();
-		
-		//Assert
-		Assert.assertEquals(selectedOption, expectedSelection);
-	}
-	
-	@Test
-	public void tc7DisappearingElements() {
-		//Arrange
-		String expectedSelection = "Disappearing Elements";
-		
-		//Act
-		String selectedOption = new DisappearingElements(driver, this.url)
-				.navigate()
-				.select()
-				.getOption();
-		
-		//Assert
-		Assert.assertEquals(selectedOption, expectedSelection);
-	}
-	
-	
-	@Test
-	public void tc9DragAndDrop() {
-		//Arrange
-		String expectedSelection = "Drag and Drop";
-		
-		//Act
-		String selectedOption = new DragAndDrop(driver, this.url)
-				.navigate()
-				.select()
-				.getOption();
-		
-		//Assert
-		Assert.assertEquals(selectedOption, expectedSelection);
-	}
-	
-	@Test
-	public void tc10CanSelectDropdownListItem() {
-		// Arrange
-		String expectedSelection = "Option 2";
-
-		// Act
-		String selectedOption = new DropdownPage(driver, this.url)
-				.navigate()
-				.select(expectedSelection)
-				.getOption();
-
-		// Assert
-		Assert.assertEquals(selectedOption, expectedSelection);
-	}
-	
-	@Test
-	public void tc11DynamicControls() {
-		//Arrange
-		String expectedSelection = "It's gone!";
-		
-		//Act
-		String selectedOption = new DynamicControls(driver, this.url)
-				.navigate()
-				.select()
-				.getOption();
-		
-		//Assert
-		Assert.assertEquals(selectedOption, expectedSelection);
-	}
-	
-	@Test
-	public void tc12DynamicLoading() {
-		//Arrange
-		String expectedSelection = "Hello World!";
-		
-		//Act
-		String selectedOption = new DynamicLoading(driver, this.url)
-				.navigate()
-				.select()
-				.getOption();
-		
-		//Assert
-		Assert.assertEquals(selectedOption, expectedSelection);
-	}
-	
-
-	
+//	@Test
+//	public void tc1canNavigateToIndexPage() {
+//		// Arrange
+//		String expectedPageTitle = "The Internet";
+//
+//		// Act
+//		String actualPageTitle = new IndexPage(driver, this.url).navigate().getTitle();
+//
+//		// Assert
+//		Assert.assertEquals(actualPageTitle, expectedPageTitle);
+//	}
+//
+//	@Test
+//	public void tc2AddRemoveElement() {
+//		// Arrange
+//		String expectedSelection = "Delete";
+//
+//		// Act
+//		String selectedOption = new AddRemoveElement(driver, this.url).navigate().select().getOption(expectedSelection);
+//		// Assert
+//		Assert.assertEquals(selectedOption, expectedSelection);
+//	}
+//
+//	@Test
+//	public void tc3BasicAuth() {
+//		// Arrange
+//		String expectedSelection = "Congratulations! You must have the proper credentials.";
+//
+//		// Act
+//		String selectedOption = new BasicAuth(driver, this.url).navigate().select().getOption();
+//
+//		// Assert
+//		Assert.assertEquals(selectedOption, expectedSelection);
+//	}
+//
+//	@Test
+//	public void tc4ChallengingDom() {
+//		// Arrange
+//		String expectedSelection = "Challenging DOM";
+//
+//		// Act
+//		String selectedOption = new ChallengingDom(driver, this.url).navigate().select().getOption();
+//
+//		// Assert
+//		Assert.assertEquals(selectedOption, expectedSelection);
+//
+//	}
+//	
+//	@Test
+//	public void tc6ContextMenu() {
+//		//Arrange
+//		String expectedSelection = "Context Menu";
+//		
+//		//Act
+//		String selectedOption = new ContextMenu(driver, this.url)
+//				.navigate()
+//				.select()
+//				.getOption();
+//		
+//		//Assert
+//		Assert.assertEquals(selectedOption, expectedSelection);
+//	}
+//	
+//	@Test
+//	public void tc7DisappearingElements() {
+//		//Arrange
+//		String expectedSelection = "Disappearing Elements";
+//		
+//		//Act
+//		String selectedOption = new DisappearingElements(driver, this.url)
+//				.navigate()
+//				.select()
+//				.getOption();
+//		
+//		//Assert
+//		Assert.assertEquals(selectedOption, expectedSelection);
+//	}
+//	
+//	
+//	@Test
+//	public void tc9DragAndDrop() {
+//		//Arrange
+//		String expectedSelection = "Drag and Drop";
+//		
+//		//Act
+//		String selectedOption = new DragAndDrop(driver, this.url)
+//				.navigate()
+//				.select()
+//				.getOption();
+//		
+//		//Assert
+//		Assert.assertEquals(selectedOption, expectedSelection);
+//	}
+//	
+//	@Test
+//	public void tc10CanSelectDropdownListItem() {
+//		// Arrange
+//		String expectedSelection = "Option 2";
+//
+//		// Act
+//		String selectedOption = new DropdownPage(driver, this.url)
+//				.navigate()
+//				.select(expectedSelection)
+//				.getOption();
+//
+//		// Assert
+//		Assert.assertEquals(selectedOption, expectedSelection);
+//	}
+//	
+//	@Test
+//	public void tc11DynamicControls() {
+//		//Arrange
+//		String expectedSelection = "It's gone!";
+//		
+//		//Act
+//		String selectedOption = new DynamicControls(driver, this.url)
+//				.navigate()
+//				.select()
+//				.getOption();
+//		
+//		//Assert
+//		Assert.assertEquals(selectedOption, expectedSelection);
+//	}
+//	
+//	@Test
+//	public void tc12DynamicLoading() {
+//		//Arrange
+//		String expectedSelection = "Hello World!";
+//		
+//		//Act
+//		String selectedOption = new DynamicLoading(driver, this.url)
+//				.navigate()
+//				.select()
+//				.getOption();
+//		
+//		//Assert
+//		Assert.assertEquals(selectedOption, expectedSelection);
+//	}
+//	
+//
+//	
 //	@Test
 //	public void tc13EntryAd() {
 //		//Arrange
@@ -198,7 +198,19 @@ public class TheInternet {
 //		Assert.assertEquals(selectedOption, expectedSelection);
 //	}
 	
-	
+//	@Test
+//	public void tc15FileDownload() {
+//		//Arrange
+//		String expectedSelection = "File Downloader";
+//		
+//		//Act
+//		String selectedOption = new FileDownload(driver,this.url)
+//					.navigate()
+//					.mapClick();
+//		
+//		//Assert
+//		Assert.assertEquals(selectedOption, expectedSelection);
+//	}
 	
 
 	@BeforeTest
